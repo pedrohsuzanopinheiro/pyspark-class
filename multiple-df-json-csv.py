2d56c36467888bf81c41169feaf3cdb7a7c7de8e
Pedro Pinheiro <pedro.hsuzanopinheiro@gmail.com>
2022-06-02 18:22:50 -0300

    uploading new data for tests

--- multiple-df-json-csv.py
+++ multiple-df-json-csv.py
@@ -1,5 +1,3 @@
-import datetime
-
 from colorama import Back, Fore, Style, init
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
@@ -9,42 +7,15 @@ init(autoreset=True)
 input_uri = "mongodb://127.0.0.1/twitter_user_timeline.guardian"
 output_uri = "mongodb://127.0.0.1/twitter_user_timeline.guardian"
 
-myspark = (
-    SparkSession.builder.appName("twitter")
-    .config("spark.mongodb.input.uri", input_uri)
-    .config("spark.mongodb.output.uri", output_uri)
-    .config(
-        "spark.jars.packages",
-        "org.mongodb.spark:mongo-spark-connector_2.12:2.4.2",
-    )
-    .getOrCreate()
+my_spark = (
+    SparkSession.builder.appName("twitter").master("local[*]").getOrCreate()
 )
 
-df = myspark.read.format("com.mongodb.spark.sql.DefaultSource").load()
-df1 = (
-    df.withColumn(
-        "created_date_cleaned",
-        f.regexp_replace(f.col("created_at"), "^[A-Za-z]+", ""),
-    )
-    .withColumn(
-        "created_timestamp",
-        f.to_timestamp(
-            f.col("created_date_cleaned"), " MMM dd HH:mm:ss Z yyyy"
-        ),
-    )
-    .withColumn(
-        "created_date",
-        f.to_date("created_timestamp", "MMM dd HH:mm:ss Z yyyy"),
-    )
-)
+file = "data/twitter_followers/the_moyc.json"
+df_followers = my_spark.read.json(file)
 
-# create view
-view_name = "twitter_user_timeline_guardian"
-df1.createOrReplaceTempView(view_name)
+file = "data/twitter_friends/the_moyc.json"
+df_friends = my_spark.read.json(file)
 
-# query
-SQL_QUERY = f"SELECT id, created_at, name, location FROM {view_name} WHERE created_date BETWEEN '2020-05-01' AND '2020-05-31'"
-df2 = myspark.sql(SQL_QUERY)
-
-for row in df2.rdd.collect():
-    print(row.created_at, row.name)
+print(Fore.WHITE + Back.MAGENTA + f"df_followers {type(df_followers)}")
+print(Fore.WHITE + Back.MAGENTA + f"df_friends {type(df_friends)}")
