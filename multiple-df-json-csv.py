7baf328d12b09c88b0829e333931ce3fc19db3a1
Pedro Pinheiro <pedro.hsuzanopinheiro@gmail.com>
2022-06-07 19:17:55 -0300

    inserting multiple data from json and mongodb to python script using spark

--- multiple-df-json-csv.py
+++ multiple-df-json-csv.py
@@ -7,7 +7,9 @@ init(autoreset=True)
 input_uri = "mongodb://127.0.0.1/twitter_user_timeline.guardian"
 output_uri = "mongodb://127.0.0.1/twitter_user_timeline.guardian"
 
-my_spark = SparkSession.builder.appName("twitter").master("local[*]").getOrCreate()
+my_spark = (
+    SparkSession.builder.appName("twitter").master("local[*]").getOrCreate()
+)
 
 file = "data/twitter_followers/the_moyc.json"
 df_followers = my_spark.read.json(file)
@@ -15,5 +17,40 @@ df_followers = my_spark.read.json(file)
 file = "data/twitter_friends/the_moyc.json"
 df_friends = my_spark.read.json(file)
 
+file = "data/netflix_titles.csv"
+df_netflix = (
+    my_spark.read.format("csv")
+    .option("inferschema", True)
+    .option("header", True)
+    .option("sep", ",")
+    .load(file)
+)
+
+dffollowers = df_followers.select(
+    "id_str",
+    "created_at",
+    "created_at_date",
+    "screen_name",
+    "followers_count",
+    "friends_count",
+)
+dffriends = df_friends.select(
+    "id_str",
+    "created_at",
+    "created_at_date",
+    "screen_name",
+    "followers_count",
+    "friends_count",
+)
+dfnetflix = df_netflix.select("show_id", "title", "cast", "release_year")
+
 print(Fore.WHITE + Back.MAGENTA + f"df_followers {type(df_followers)}")
+print(dffollowers.show())
+
 print(Fore.WHITE + Back.MAGENTA + f"df_friends {type(df_friends)}")
+print(dffriends.show())
+
+print(Fore.WHITE + Back.MAGENTA + f"df_netflix {type(df_netflix)}")
+print(dfnetflix.show())
+
+my_spark.stop()
